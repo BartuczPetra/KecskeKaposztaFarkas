8a68228dc17d63c5667939d53195eb7ec6ef3201
BartuczPetra <bartucz.petra@gmail.com>
2021-04-07 11:05:16 +0200

    5. és 6. feladat

--- public_html/kecske.js
+++ public_html/kecske.js
@@ -10,12 +10,21 @@ var kepTomb = [];
 function init(){
     for (var i = 0; i < $("#bal img").length; i++) {
         $("#bal img")[i].addEventListener("click", feladat1);
+        $("#bal img")[i].addEventListener("mouseover", feladat5);
+        $("#bal img")[i].addEventListener("mouseout", feladat6);
     }
-   
+   $("footer p")[0].innerHTML="Balogh Bíborka, Kiss Niki, Bartucz Petra";
 }
 function feladat1(){
      kepTomb.push(this.src);
      console.log(kepTomb);
-    $("footer p")[0].innerHTML="Balogh Bíborka, Kiss Niki, Bartucz Petra";
+    
+}
+
+function feladat5(){
+    this.classList.add("kiemel")
+}
+function feladat6(){
+    this.classList.remove("kiemel")
 }
 
