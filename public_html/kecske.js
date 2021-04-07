9998b383b5652d1d78b5bd80c365af3dd987e9fd
Niko <kissnikolettanett@gmail.com>
2021-04-07 10:58:57 +0200

    js modosit 2

--- public_html/kecske.js
+++ public_html/kecske.js
@@ -11,11 +11,10 @@ function init(){
     for (var i = 0; i < $("#bal img").length; i++) {
         $("#bal img")[i].addEventListener("click", feladat1);
     }
-   
+   $("footer p")[0].innerHTML="Balogh Bíborka, Kiss Niki, Bartucz Petra";
 }
 function feladat1(){
      kepTomb.push(this.src);
      console.log(kepTomb);
-    $("footer p")[0].innerHTML="Balogh Bíborka, Kiss Niki, Bartucz Petra";
 }
 
