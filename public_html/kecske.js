8c496c0ee530ecc5737fbad7b6497824ac6ff135
bibor-01 <balogh01biborka@gmail.com>
2021-04-07 12:19:38 +0200

    7feladat

--- public_html/kecske.js
+++ public_html/kecske.js
@@ -7,15 +7,23 @@ function $(nev) {
     return document.querySelectorAll(nev);
 }
 var kepTomb = [];
+
 function init(){
     for (var i = 0; i < $("#bal img").length; i++) {
-        $("#bal img")[i].addEventListener("click", feladat1);
+        $("#bal img")[i].addEventListener("click", feladat3);
     }
-   
+   $("footer p")[0].innerHTML="Balogh Bíborka, Kiss Niki, Bartucz Petra";
 }
-function feladat1(){
+function feladat3(){
      kepTomb.push(this.src);
      console.log(kepTomb);
-    $("footer p")[0].innerHTML="Balogh Bíborka, Kiss Niki, Bartucz Petra";
+     //this.style.display = "none";
+     $("article div").innerHTML += $("#bal img");
+     this.style.order = "-10";
+     
+ 
+     
+     
+    
 }
 
