4c518707bde8518d1d6ff773a8492a38cb9edd41
bibor-01 <balogh01biborka@gmail.com>
2021-04-07 10:48:54 +0200

    3feladat

--- public_html/javascript.js
+++ public_html/javascript.js
@@ -6,8 +6,15 @@ function ID(nev) {
 function $(nev) {
     return document.querySelectorAll(nev);
 }
+var kepTomb = [];
 function init(){
-    
+    for (var i = 0; i < $("#bal img").length; i++) {
+        $("#bal img")[i].addEventListener("click", feladat1);
+    }
+   
 }
-
+function feladat1(){
+     kepTomb.push(this.src);
+     console.log(kepTomb);
+}
 
