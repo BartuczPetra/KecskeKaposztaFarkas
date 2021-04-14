5a0f4611c7e2e996dbb1f9480b4dbba26aaad7e9
Niko <kissnikolettanett@gmail.com>
2021-04-14 10:06:11 +0200

    8 feladat befejezve

--- public_html/kecske.js
+++ public_html/kecske.js
@@ -8,32 +8,37 @@ function $(nev) {
 }
 var kepTomb = [];
 
-function init(){
+function init() {
     for (var i = 0; i < $("#bal img").length; i++) {
         $("#bal img")[i].addEventListener("click", feladat3);
         $("#bal img")[i].addEventListener("mouseover", feladat5);
         $("#bal img")[i].addEventListener("mouseout", feladat6);
     }
-   $("footer p")[0].innerHTML="Balogh Bíborka, Kiss Niki, Bartucz Petra";
-}
-function feladat3(){
-     kepTomb.push(this.src);
-     console.log(kepTomb);
-     this.style.display = "none";
-    ID("csonak").innerHTML +='<img src="'+this.src+'" alt="utas" style="position: relative; top:200px; right: 0; width: 50px; height:50px; z-index: 10;">';
+    $("footer p")[0].innerHTML = "Balogh Bíborka, Kiss Niki, Bartucz Petra";
+    //feladat8();
 }
 
-function feladat5(){
+function feladat3() {
+    kepTomb.push(this.src);
+    console.log(kepTomb);
+    //gitgitthis.style.display = "none";
+    $("article div").innerHTML += $("#bal img");
+    this.style.order = "-10";
+    console.log(kepTomb);
+    this.style.display = "none";
+    ID("csonak").innerHTML += '<img src="' + this.src + '" alt="utas" style="position: relative; top:200px; right: 0; width: 50px; height:50px; z-index: 10;">';
+    feladat8();
+}
+
+function feladat5() {
     this.classList.add("kiemel");
 }
-function feladat6(){
+function feladat6() {
     this.classList.remove("kiemel");
 }
 
-function feladat8(){
-    if ("#bal p:first-child" === "#bal p:nth-child(2)"){
-        alert("Nem jó! Megesziii!");
-    }else if ("#bal p:first-child" === "#bal p:nth-child(3)"){
-        alert("Nem jó! Megesziii!");
+function feladat8() {
+    if (kepTomb.includes("http://localhost:8383/Kecske_Kaposzta_Farkas/kepek/kecske2.png") && kepTomb.includes("http://localhost:8383/Kecske_Kaposzta_Farkas/kepek/kaposzta.png")){
+        alert("Nem jó, mert megeszi!");
     }
 }
